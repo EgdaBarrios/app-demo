c3af3536d5a6c5235c771e05cd401f9173d538eb
Egda Barrios <egdabarrios@gmail.com>
2021-06-23 17:26:01 -0500

    driver list template

--- src/app/dashboard/drivers/drivers-routing.module.ts
+++ src/app/dashboard/drivers/drivers-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DriverListComponent } from './driver-list/driver-list.component';
+import { DriverEditComponent } from './driver-edit/driver-edit.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'drivers',
         component: DriverListComponent
+      },
+      {
+        path: 'driver-edit/:id',
+        component: DriverEditComponent
       }
     ]
   }
--- src/app/dashboard/drivers/drivers.module.ts
+++ src/app/dashboard/drivers/drivers.module.ts
@@ -6,12 +6,14 @@ import { DriverListComponent } from './driver-list/driver-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DriversRoutingModule } from './drivers-routing.module';
+import { DriverEditComponent } from './driver-edit/driver-edit.component';
 
 
 
 @NgModule({
   declarations: [
-    DriverListComponent
+    DriverListComponent,
+    DriverEditComponent
   ],
   imports: [
     CommonModule,
